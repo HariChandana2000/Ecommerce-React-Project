79e1bc7d71909adf1f0ee27056be4744eeb6d5fa
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-10-30 22:16:51 +0530

    redux store setup

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+const { combineReducers, configureStore } = require("@reduxjs/toolkit");
+
+const rootReducer = combineReducers();
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;
