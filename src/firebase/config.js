0edfd5def1bd3c55b76c2a8aafd9ca1b65947c2d
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-10-30 13:13:55 +0530

    updated for firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJ1E86MpUFkJGWksQ0l-ZX54CT-8UohPA",
+  authDomain: "eshop-bdf68.firebaseapp.com",
+  projectId: "eshop-bdf68",
+  storageBucket: "eshop-bdf68.appspot.com",
+  messagingSenderId: "806021401704",
+  appId: "1:806021401704:web:fb3041e658337042d3ffdd",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
