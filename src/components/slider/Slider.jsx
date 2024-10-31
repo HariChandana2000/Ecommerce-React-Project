3064cae934e94f9738c10b8517b7c34e9817a1bc
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-10-31 13:20:06 +0530

    updated for auto scrolling in slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Slider.scss";
 import { sliderData } from "./slider-data";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
@@ -7,12 +7,32 @@ const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideLength = sliderData.length;
 
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 5000;
+
   const nextSlide = () => {
     setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
   };
+
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
   };
+
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
+
+  function auto() {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+
+  useEffect(() => {
+    if (autoScroll) {
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [currentSlide]);
 
   return (
     <div className='slider'>
