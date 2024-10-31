b3aedc5c433d50a4fb5e777ee8da4546e66df498
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-10-31 13:06:08 +0530

    created slider

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./Slider.scss";
+import { sliderData } from "./slider-data";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  };
+
+  return (
+    <div className='slider'>
+      <AiOutlineArrowLeft className='arrow prev' onClick={prevSlide} />
+      <AiOutlineArrowRight className='arrow next' onClick={nextSlide} />
+
+      {sliderData.map((slide, index) => {
+        const { image, heading, desc } = slide;
+        return (
+          <div
+            key={index}
+            className={index === currentSlide ? "slide current" : "slide"}
+          >
+            {index === currentSlide && (
+              <>
+                <img src={image} alt='slide' />
+                <div className='content'>
+                  <h2>{heading}</h2>
+                  <p>{desc}</p>
+                  <hr />
+                  <a href='#products' className='--btn --btn-primary'>
+                    Shop Now
+                  </a>
+                </div>
+              </>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Home.module.scss";
+import Slider from "../../components/slider/Slider";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Slider />
     </div>
   );
 };
