ac5ccdda2944e318c41b38a247ff0528f5eaeff4
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-10-31 15:51:57 +0530

    created admin only route

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,6 +12,7 @@ import {
   SET_ACTIVE_USER,
 } from "../../redux/slice/authSlice";
 import ShowOnLogin, { ShowOnLogout } from "../hiddenLinks/HiddenLink";
+import AdminOnlyRoute from "../adminOnlyRoute/AdminOnlyRoute";
 
 const logo = (
   <div className={styles.logo}>
@@ -110,6 +111,11 @@ const Header = () => {
             <li className={styles["logo-mobile"]}>
               {logo}
               <FaTimes size={22} color='#fff' onClick={hideMenu} />
+            </li>
+            <li>
+              <AdminOnlyRoute>
+                <button className='--btn --btn-primary'>Admin</button>
+              </AdminOnlyRoute>
             </li>
             <li>
               <NavLink to='/' className={activeLink}>
