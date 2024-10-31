efb0065874d36aa83ffe9ba91e196242eaa8c043
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-10-31 21:11:37 +0530

    get products from firestore

--- src/components/admin/viewProducts/ViewProducts.jsx
+++ src/components/admin/viewProducts/ViewProducts.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ViewProducts.module.scss";
+import { toast } from "react-toastify";
+import { db } from "../../../firebase/config";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 
 const ViewProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getProducts = () => {
+    setIsLoading(true);
+
+    try {
+      const productsRef = collection(db, "products");
+
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setProducts(allProducts);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return <div>ViewProducts</div>;
 };
 
