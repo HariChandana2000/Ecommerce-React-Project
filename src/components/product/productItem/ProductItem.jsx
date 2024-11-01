0aa15e787a1d4de513ad058c30d47090c295b0fc
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-01 13:35:04 +0530

    updated logic for product item component

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import styles from "./ProductItem.module.scss";
+import Card from "../../card/Card";
+import { Link } from "react-router-dom";
 
-const ProductItem = () => {
-  return <div>ProductItem</div>;
+const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      return text.substring(0, n).concat("...");
+    }
+    return text;
+  };
+
+  return (
+    <Card cardClass={grid ? styles.grid : styles.list}>
+      <Link to={`/product-details/${id}`}>
+        <div className={styles.img}>
+          <img src={imageURL} alt={name} />
+        </div>
+      </Link>
+
+      <div className={styles.content}>
+        <div className={styles.details}>
+          <p>${price}</p>
+          <h4>{grid ? shortenText(name, 18) : name}</h4>
+        </div>
+        {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
+        <button className='--btn --btn-danger'>Add To Cart</button>
+      </div>
+    </Card>
+  );
 };
 
 export default ProductItem;
