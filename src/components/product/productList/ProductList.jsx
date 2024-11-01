dbafc31b6501bec1c3d041cd5f66ffb42cd1772d
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-01 13:16:56 +0530

    added logic for product list

--- src/components/product/productList/ProductList.jsx
+++ src/components/product/productList/ProductList.jsx
@@ -3,9 +3,11 @@ import styles from "./ProductList.module.scss";
 import { BsFillGridFill } from "react-icons/bs";
 import { FaListAlt } from "react-icons/fa";
 import Search from "../../search/Search";
+import ProductItem from "../productItem/ProductItem";
 
 const ProductList = ({ products }) => {
   const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState("");
 
   return (
     <div className={styles["product-list"]} id='product'>
@@ -24,7 +26,7 @@ const ProductList = ({ products }) => {
 
         {/* Search */}
         <div>
-          <Search />
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
         </div>
 
         {/* Sort Products */}
@@ -38,6 +40,22 @@ const ProductList = ({ products }) => {
             <option value='z-a'>Z-A</option>
           </select>
         </div>
+      </div>
+
+      <div className={grid ? styles.grid : styles.list}>
+        {products.length === 0 ? (
+          <p>No products found!</p>
+        ) : (
+          <>
+            {products.map((product) => {
+              return (
+                <div key={product.id}>
+                  <ProductItem {...product} grid={grid} product={product} />
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
     </div>
   );
