ef6858e92bea040ffe5bbcdf4af1b50a47913840
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-01 17:38:25 +0530

    filter products by category

--- src/components/product/productFilter/ProductFilter.jsx
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProductFilter.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { selectProducts } from "../../../redux/slice/productSlice";
+import { FILTER_BY_CATEGORY } from "../../../redux/slice/filterSlice";
 
 const ProductFilter = () => {
+  const products = useSelector(selectProducts);
+  const [category, setCategory] = useState("All");
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filterProducts = (cat) => {
+    setCategory(cat);
+    dispatch(FILTER_BY_CATEGORY({ products, category: cat }));
+  };
+
   return (
     <div className={styles.filter}>
       <h4>Categrories</h4>
       <div className={styles.category}>
-        <button>All</button>
+        {allCategories.map((cat, index) => {
+          return (
+            <button
+              key={index}
+              type='button'
+              className={`${category}` === cat ? styles.active : null}
+              onClick={() => filterProducts(cat)}
+            >
+              &#8250;{cat}
+            </button>
+          );
+        })}
       </div>
       <h4>Brand</h4>
       <div className={styles.brand}>
