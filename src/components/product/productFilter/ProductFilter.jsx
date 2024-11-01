eded2fbe816ae96734e47e8b458056328b4fb894
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-01 16:31:44 +0530

    setup product filter

--- src/components/product/productFilter/ProductFilter.jsx
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import styles from "./ProductFilter.module.scss";
 
 const ProductFilter = () => {
-  return <div>ProductFilter</div>;
+  return (
+    <div className={styles.filter}>
+      <h4>Categrories</h4>
+      <div className={styles.category}>
+        <button>All</button>
+      </div>
+      <h4>Brand</h4>
+      <div className={styles.brand}>
+        <select name='brand'>
+          <option value='all'>All</option>
+        </select>
+      </div>
+      <h4>Price</h4>
+      <p>$1500</p>
+      <div className={styles.price}>
+        <input type='range' name='price' min={100} max={1000} />
+      </div>
+      <br />
+      <button className='--btn --btn-danger'>Clear Filters</button>
+    </div>
+  );
 };
 
 export default ProductFilter;
