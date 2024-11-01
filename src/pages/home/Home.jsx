067c04a9a47e9744ac9e21eea122cc031d46ce27
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-01 14:34:46 +0530

    updated code to scroll down to products when clicked on the button

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Home.module.scss";
 import Slider from "../../components/slider/Slider";
 import Product from "../../components/product/Product";
 
 const Home = () => {
+  const url = window.location.href;
+
+  const scrollToProducts = () => {
+    if (url.includes("#products")) {
+      window.scrollTo({
+        top: 700,
+        behavior: "smooth",
+      });
+      return;
+    }
+  };
+
+  useEffect(() => {
+    scrollToProducts();
+  }, []);
+
   return (
     <div>
       <Slider />
