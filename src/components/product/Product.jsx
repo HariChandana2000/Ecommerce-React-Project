e9212b97637922d037ea3886a7bda525de72a06d
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-01 11:52:31 +0530

    fetched products from product component

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Product.module.scss";
 import ProductFilter from "./productFilter/ProductFilter";
 import ProductList from "./productList/ProductList";
+import useFetchCollection from "../../customHooks/useFetchCollection";
+import { useDispatch, useSelector } from "react-redux";
+import { selectProducts, STORE_PRODUCTS } from "../../redux/slice/productSlice";
 
 const Product = () => {
+  const { data, isLoading } = useFetchCollection("products");
+
+  const dispatch = useDispatch();
+
+  const products = useSelector(selectProducts);
+
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
+  }, [data, dispatch]);
+
   return (
     <section>
       <div className={`container ${styles.product}`}>
@@ -11,7 +28,7 @@ const Product = () => {
           <ProductFilter />
         </aside>
         <div className={styles.content}>
-          <ProductList></ProductList>
+          <ProductList products={products} />
         </div>
       </div>
     </section>
