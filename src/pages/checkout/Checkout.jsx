305c66bc4f602f34015e140a2b9fe16efa258ef4
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-02 13:46:42 +0530

    created checkout components

---
+++ src/pages/checkout/Checkout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CheckoutDetails.module.scss";
+
+const Checkout = () => {
+  return <div>Checkout</div>;
+};
+
+export default Checkout;
---
+++ src/pages/checkout/CheckoutDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./CheckoutDetails.module.scss";
+
+const CheckoutDetails = () => {
+  return (
+    <div>
+      <h2>Checkout Details</h2>
+    </div>
+  );
+};
+
+export default CheckoutDetails;
