9ce2553bdeab73338d0391b4182a279930bd1370
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-03 12:06:17 +0530

    created components from admin

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Char.module.scss";
+
+const Chart = () => {
+  return <div>Chart</div>;
+};
+
+export default Chart;
---
+++ src/components/infoBox/InfoBox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./InfoBox.module.scss";
+
+const InfoBox = () => {
+  return <div>InfoBox</div>;
+};
+
+export default InfoBox;
