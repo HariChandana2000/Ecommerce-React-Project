4869711fbbf6a6e33f3ff728dff8cee01c03fef8
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-03 11:06:11 +0530

    save review to firebase

--- src/components/reviewProducts/ReviewProducts.jsx
+++ src/components/reviewProducts/ReviewProducts.jsx
@@ -1,38 +1,71 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ReviewProducts.module.scss";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectProducts } from "../../redux/slice/productSlice";
 import { selectUserId, selectUsername } from "../../redux/slice/authSlice";
 import Card from "../card/Card";
 import StarsRating from "react-star-rate";
+import { toast } from "react-toastify";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { db } from "../../firebase/config";
+import useFetchDocument from "../../customHooks/useFetchDocument";
+import spinnerImg from "../../assets/spinner.jpg";
 
 const ReviewProducts = () => {
   const { id } = useParams();
   const [rate, setRate] = useState(0);
   const [review, setReview] = useState("");
-  const products = useSelector(selectProducts);
+  const { document } = useFetchDocument("products", id);
+  const [product, setProduct] = useState(null);
   const userId = useSelector(selectUserId);
   const userName = useSelector(selectUsername);
 
-  const product = products.find((item) => item.id === id);
+  useEffect(() => {
+    setProduct(document);
+  }, [document]);
 
   const submitReview = (e) => {
     e.preventDefault();
+    const today = new Date();
+    const date = today.toDateString();
+    const reviewConfig = {
+      userId,
+      userName,
+      productId: id,
+      rate,
+      review,
+      reviewDate: date,
+      createdAt: Timestamp.now().toDate(),
+    };
+
+    try {
+      addDoc(collection(db, "reviews"), reviewConfig);
+      toast.success("Review submitted successfully");
+      setRate(0);
+      setReview("");
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
     <section>
       <div className={`container ${styles.review}`}>
         <h2>Rate This Product</h2>
-        <p>
-          <b>Product Name:</b> {product.name}
-        </p>
-        <img
-          src={product.imageURL}
-          alt={product.name}
-          style={{ width: "100px" }}
-        />
+        {product === null ? (
+          <img src={spinnerImg} alt='spinner' style={{ width: "50px" }} />
+        ) : (
+          <>
+            <p>
+              <b>Product Name:</b> {product.name}
+            </p>
+            <img
+              src={product.imageURL}
+              alt={product.name}
+              style={{ width: "100px" }}
+            />
+          </>
+        )}
         <Card cardClass={styles.card}>
           <form onSubmit={(e) => submitReview(e)}>
             <label>Rating:</label>
