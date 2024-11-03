74aabfc6a8a97df1d82fcf859b9c08f743995afc
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-11-03 13:01:14 +0530

    view all orders on admin dashboard

--- src/components/admin/orders/Orders.jsx
+++ src/components/admin/orders/Orders.jsx
@@ -1,8 +1,94 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Orders.module.scss";
+import useFetchCollection from "../../../customHooks/useFetchCollection";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectOrderHistory,
+  STORE_ORDERS,
+} from "../../../redux/slice/orderSlice";
+import { selectUserId } from "../../../redux/slice/authSlice";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../loader/Loader";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { data, isLoading } = useFetchCollection("orders");
+  const orders = useSelector(selectOrderHistory);
+  const userId = useSelector(selectUserId);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(STORE_ORDERS(data));
+  }, [dispatch, data]);
+
+  const handleClick = (id) => {
+    navigate(`/admin/order-details/${id}`);
+  };
+
+  return (
+    <>
+      <div className={`${styles.order}`}>
+        <h2>All Orders</h2>
+        <p>
+          Open an order to <b>Change Order Status</b>
+        </p>
+        <br />
+        <>
+          {isLoading && <Loader />}
+          <div className={styles.table}>
+            {orders.length === 0 ? (
+              <p>No order found</p>
+            ) : (
+              <table>
+                <thead>
+                  <tr>
+                    <th>s/n</th>
+                    <th>Date</th>
+                    <th>Order ID</th>
+                    <th>Order Amount</th>
+                    <th>Order Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order, index) => {
+                    const {
+                      id,
+                      orderDate,
+                      orderTime,
+                      orderAmount,
+                      orderStatus,
+                    } = order;
+                    return (
+                      <tr key={id} onClick={() => handleClick(id)}>
+                        <td>{index + 1}</td>
+                        <td>
+                          {orderDate} at {orderTime}
+                        </td>
+                        <td>{id}</td>
+                        <td>${orderAmount}</td>
+                        <td>
+                          <p
+                            className={
+                              orderStatus !== "Delivered"
+                                ? `${styles.pending}`
+                                : `${styles.delivered}`
+                            }
+                          >
+                            {orderStatus}
+                          </p>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </>
+      </div>
+    </>
+  );
 };
 
 export default Orders;
